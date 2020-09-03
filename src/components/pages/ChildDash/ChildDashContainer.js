b5aad79c745966649abb1b79e0448cffbebf9f93
unknown <dethgrenade@gmail.com>
2020-09-03 14:08:31 -0700

    added ChildDash skeleton components

---
+++ src/components/pages/ChildDash/ChildDashContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import RenderChildDash from './RenderChildDash';
+
+const ChildDashContainer = () => {
+  return <RenderChildDash />;
+};
+
+export default ChildDashContainer;
