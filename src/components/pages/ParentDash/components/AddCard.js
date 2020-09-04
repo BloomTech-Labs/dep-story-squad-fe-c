6dd80723ce0124526fc940df773cf8c8ce69bf1d
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-09-04 15:02:43 -0700

    created a "add child" button, started working on the add child form as well. need to finish styling and implement functionality with DB

---
+++ src/components/pages/ParentDash/components/AddCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../dash.css';
+
+const AddCard = () => {
+  return (
+    <button className="AddCard">
+      <h1>Add a Child</h1>
+    </button>
+  );
+};
+
+export default AddCard;
---
+++ src/components/pages/ParentDash/components/index.js
@@ -0,0 +1,3 @@
+export { default as Nav } from './Nav.js';
+export { default as AddCard } from './AddCard.js';
+export { default as ChildSignup } from './ChildSignup.js';
