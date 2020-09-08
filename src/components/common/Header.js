f92d84197599a7491779b0cca6fc6cfb7ad03abb
unknown <dethgrenade@gmail.com>
2020-09-08 16:04:00 -0700

    added <img> of sharck city to Header common component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,6 +4,7 @@ const Header = ({ title }) => {
   return (
     <header className="storysquad-header">
       <h1 className="header-title">{title}</h1>
+      <img src="/images/header-image.png" alt="underwater shark cityscape" />
     </header>
   );
 };
