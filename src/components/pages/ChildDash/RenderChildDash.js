23b620ec31e586945c0cf9a3e4b3aa0f0c2ce489
unknown <dethgrenade@gmail.com>
2020-09-08 12:44:04 -0700

    added <div> containers and <h2> titles to RenderChildDash component

--- src/components/pages/ChildDash/RenderChildDash.js
+++ src/components/pages/ChildDash/RenderChildDash.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const RenderChildDash = () => {
-  return <></>;
+  return (
+    <div>
+      <div className="dash-btn-container mission">
+        <h2>Accept The Mission!</h2>
+      </div>
+      <div className="dash-btn-container avatar">
+        <h2>Change Your Avatar</h2>
+      </div>
+      <div className="dash-btn-container passport">
+        <h2>Adventure Passport</h2>
+      </div>
+      <div className="dash-btn-container trophy">
+        <h2>Tropy Room</h2>
+      </div>
+    </div>
+  );
 };
 
 export default RenderChildDash;
