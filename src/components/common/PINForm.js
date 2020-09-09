c37b6b20faff4219b55c3c9c81efa77ae53d3fd5
fuston05 <fuston05@yahoo.com>
2020-09-09 16:14:56 -0400

    adds PIN form component, adds react-pin-input npm package, adds a font-familyt to theme-overrides.js

--- src/components/common/PINForm.js
+++ src/components/common/PINForm.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import PinInput from 'react-pin-input';
+
+import { Form, Input } from 'antd';
+
+// styles
+import './PINForm.less';
+
 function PINForm(props) {
-  return <div></div>;
+  const [formValue, setFormValue] = useState();
+
+  const onFinish = () => {
+    return;
+  };
+
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
+  const handleChange = value => {
+    console.log(value);
+    return setFormValue(value);
+  };
+
+  const handleSubmit = () => {
+    console.log('submitted!!!');
+    // need an axios call here to the back-end
+    return;
+  };
+
+  return (
+    <div>
+      <Form
+        className="pinForm"
+        layout={layout}
+        name="control-hooks"
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="PIN"
+          label="PIN"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <PinInput
+            length={4}
+            initialValue=""
+            // secret= {false}
+            onChange={(value, index) => {
+              handleChange(value);
+            }}
+            type="numeric"
+            inputMode="number"
+            style={{ padding: '10px' }}
+            inputStyle={{ borderColor: 'blue' }}
+            inputFocusStyle={{ borderColor: 'darkBlue' }}
+            onComplete={(value, index) => {
+              handleSubmit();
+            }}
+            autoSelect={true}
+          />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 PINForm.propTypes = {};
