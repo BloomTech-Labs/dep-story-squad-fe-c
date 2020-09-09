b8d46e687aac581c993b49809a1219ea96ed079f
fuston05 <fuston05@yahoo.com>
2020-09-09 15:01:50 -0400

    adds empty PIN form component, with Less file, adds import/export to the index.js common components dir

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -4,6 +4,15 @@ import List from './List';
 import LoadingComponent from './LoadingComponent';
 import Button from './Button';
 import Header from './Header';
+import PINForm from './PINForm';
 // notice we're building out a 'package' of reusables here and exporting them as an object of component properties.
 // to use this, simply `import {foo, bar, baz} from '<path-to-this-directory>/ReusableComponents';`
-export { FormButton, FormInput, List, LoadingComponent, Button, Header };
+export {
+  FormButton,
+  FormInput,
+  List,
+  LoadingComponent,
+  Button,
+  Header,
+  PINForm,
+};
