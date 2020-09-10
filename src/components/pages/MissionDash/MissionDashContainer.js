17f406f3b622f0e06360a2dd1b6f6ba2ba61484a
unknown <dethgrenade@gmail.com>
2020-09-10 14:00:56 -0700

    created initial files and skeleton of component structure for Mission Dashboard

---
+++ src/components/pages/MissionDash/MissionDashContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import RenderMissionDash from './RenderMissionDash';
+
+import './styles/less';
+
+const MissionDashContainer = () => {
+  return <RenderMissionDash />;
+};
+
+export default MissionDashContainer;
