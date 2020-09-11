1bb060a3183a24a132b97ef9a9e0aa4de2b98cea
fuston05 <fuston05@yahoo.com>
2020-09-11 19:43:47 -0400

    got modal working properly with the pinForm, PinForm now has access to modal state, modal accepts and passes state to its children

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -5,7 +5,7 @@ import LoadingComponent from './LoadingComponent';
 import Button from './Button';
 import Header from './Header';
 import PINForm from './PINForm';
-import ModalComp from './ModalComp';
+import ModalComp from './FormModalComp';
 // notice we're building out a 'package' of reusables here and exporting them as an object of component properties.
 // to use this, simply `import {foo, bar, baz} from '<path-to-this-directory>/ReusableComponents';`
 export {
