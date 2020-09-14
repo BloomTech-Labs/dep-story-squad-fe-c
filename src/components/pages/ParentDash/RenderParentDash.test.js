c41801574581c6b4475cee8613cb30ea933f3eb2
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-09-14 14:54:30 -0700

    fixed className in signup form to resolve warnings

--- src/components/pages/ParentDash/RenderParentDash.test.js
+++ src/components/pages/ParentDash/RenderParentDash.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import RenderParentDash from './RenderParentDash';
-import { DashHome, LinkButton, ParentNav } from './components';
+import { DashHome, LinkButton, ParentNav, ChildSignup } from './components';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 test('ParentDash renders without errors', () => {
@@ -28,6 +28,14 @@ test('LinkButton renders without errors', () => {
   render(
     <Router>
       <LinkButton to="/login/add">Here's a Button</LinkButton>
+    </Router>
+  );
+});
+
+test('Child signup form renders without errors', () => {
+  render(
+    <Router>
+      <ChildSignup />
     </Router>
   );
 });
