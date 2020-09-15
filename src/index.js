6df79ddf3c467a7f5b64c1b33d07540840c74805
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-09-15 15:26:48 -0700

    removed unused components from index.js

--- src/index.js
+++ src/index.js
@@ -11,16 +11,11 @@ import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import 'antd/dist/antd.less';
 
 import { NotFoundPage } from './components/pages/NotFound';
-import { ExampleListPage } from './components/pages/ExampleList';
-import { ProfileListPage } from './components/pages/ProfileList';
 import { LoginPage } from './components/pages/Login';
-import { HomePage } from './components/pages/Home';
-import { ExampleDataViz } from './components/pages/ExampleDataViz';
 import { config } from './utils/oktaConfig';
 import { LoadingComponent } from './components/common';
 import { Dash } from './components/pages/Dash';
 
-import { ParentDash } from './components/pages/ParentDash';
 import { FormModalComp } from './components/common';
 import { MissionDash } from './components/pages/MissionDash';
 import UserForm from './components/common/UserForm';
@@ -49,14 +44,14 @@ function App() {
 
   // This state will hold the title that appears in the <Header />
   const [headerTitle, setHeaderTitle] = useState('Story Squad');
+  const [userType, setUserType] = useState(null);
 
   return (
     <Security {...config} onAuthRequired={authHandler}>
       <Header title={headerTitle} />
       <Switch>
         <Route path="/login" component={LoginPage} />
-        <Route path="/mission" component={MissionDash} />
-        {/* <Route path="/user-form" component={ModalComp} /> */}
+
         <SecureRoute path="/implicit/callback">
           {' '}
           <LoginCallback />
@@ -67,13 +62,19 @@ function App() {
           exact
           component={() => (
             <FormModalComp>
-              <UserForm />
+              <UserForm setUserType={setUserType} />
             </FormModalComp>
           )}
         />
         <SecureRoute
           path="/dashboard"
-          component={() => <Dash setHeaderTitle={setHeaderTitle} />}
+          component={() => (
+            <Dash setHeaderTitle={setHeaderTitle} userType={userType} />
+          )}
+        />
+        <SecureRoute
+          path="/mission"
+          component={() => <MissionDash setHeaderTitle={setHeaderTitle} />}
         />
         <Route component={NotFoundPage} />
       </Switch>
