ff94ec2aec372fb9b9811e8e9c8caf6e8f611067
dawsonhammdev <hammsdesigns@gmail.com>
2020-09-15 14:38:39 -0500

    I have added a useHistory to push the HandleCancel fuction to the /login when the X is clicked

--- src/components/common/FormModalComp.js
+++ src/components/common/FormModalComp.js
@@ -2,6 +2,7 @@
 
 import React, { useState, Children, cloneElement } from 'react';
 import { Modal } from 'antd';
+import { useHistory } from 'react-router-dom';
 
 // styles
 import './FormModalComp.less';
@@ -9,6 +10,7 @@ import PropTypes from 'prop-types';
 
 const ModalComp = props => {
   const [showModal, setShowModal] = useState(true);
+  const history = useHistory();
 
   // stores 'formdata' from each form in form sequence til ready for submission. formsubmissionData ex:
   // {
@@ -30,6 +32,7 @@ const ModalComp = props => {
   }
 
   const handleCancel = () => {
+    history.push('/login');
     setShowModal(false);
   };
 
