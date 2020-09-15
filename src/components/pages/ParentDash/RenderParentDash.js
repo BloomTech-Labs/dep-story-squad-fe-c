ac80d96e3e74aed599f8fc8fd9a33cde0e862deb
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-09-15 13:14:10 -0700

    added small changes to allow situational render of components with side nav

--- src/components/pages/ParentDash/RenderParentDash.js
+++ src/components/pages/ParentDash/RenderParentDash.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import './dash.css';
 
@@ -7,12 +7,33 @@ import { ParentNav, DashHome, ChildSignup } from './components';
 
 // ParentDash component that contains a nav bar and routes to the various components
 const RenderParentDash = () => {
+  const [current, setCurrent] = useState('home');
+
+  useEffect(() => {}, [current]);
+
+  const currentPage = () => {
+    if (current === 'home') {
+      return (
+        <>
+          <Route exact path={'/login'} component={DashHome} />
+          <Route path={`/login/add`} component={ChildSignup} />
+        </>
+      );
+    }
+  };
+  const handleClick = e => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
   return (
     <>
       <Layout style={{ background: 'white', height: '100vh' }}>
-        <ParentNav />
-        <Route exact path={'/login'} component={DashHome} />
-        <Route path={`/login/add`} component={ChildSignup} />
+        <ParentNav
+          handleClick={handleClick}
+          current={current}
+          setCurrent={setCurrent}
+        />
+        {currentPage()}
       </Layout>
     </>
   );
