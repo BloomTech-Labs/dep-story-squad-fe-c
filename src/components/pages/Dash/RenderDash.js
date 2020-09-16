cf263daf5d3a7582119386eb35edac9d7211ffec
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-09-16 13:59:59 -0700

    refactored dash component for faster rendering

--- src/components/pages/Dash/RenderDash.js
+++ src/components/pages/Dash/RenderDash.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { ParentDash } from '../ParentDash';
 import { ChildDash } from '../ChildDash';
 
-const RenderDash = props => {
-  const currentPage = () => {
-    if (props.current === 'parent') {
-      return <ParentDash />;
-    } else if (props.current === 'child') {
-      return <ChildDash />;
-    } else {
-      return null;
-    }
-  };
+const dash = {
+  parent: <ParentDash />,
+  child: <ChildDash />,
+  null: null,
+};
 
-  return <>{currentPage()}</>;
+const RenderDash = props => {
+  return <>{dash[props.userType]}</>;
 };
 
 export default RenderDash;
