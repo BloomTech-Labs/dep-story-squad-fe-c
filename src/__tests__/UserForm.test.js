b3336c0677e2e59ce6b5f4ecaf99c1a5a3347aa6
fuston05 <fuston05@yahoo.com>
2020-09-18 16:22:22 -0400

    changed UserForm test to accomidate changes to the UserForm

--- src/__tests__/UserForm.test.js
+++ src/__tests__/UserForm.test.js
@@ -4,7 +4,9 @@ import UserForm from '../components/common/accountPinModal/UserForm';
 
 describe('UserForm component', () => {
   test('renders in the dom', () => {
-    const { getByText } = render(<UserForm />);
+    const { getByText } = render(
+      <UserForm userAccounts={['Dad', 'Child1', 'Child2']} />
+    );
     expect(
       getByText(
         /So we can direct you to the right place, please let us know who you are./i
