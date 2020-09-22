ddadf3079f857f20b7752803e82ae1c97f87010f
unknown <dethgrenade@gmail.com>
2020-09-22 09:26:35 -0700

    refactored message-popup.js in utils/ for readability

--- src/utils/message-popup.js
+++ src/utils/message-popup.js
@@ -3,11 +3,7 @@ import { Modal } from 'antd';
 
 // displays a modal with a message to the user
 export const messagePopup = message => {
-  return Modal.info({
-    content: (
-      <div>
-        <p>{message}</p>
-      </div>
-    ),
+  return Modal.warning({
+    content: message,
   });
 };
