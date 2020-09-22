0e890d902a24525b06c65f03b4965332b8a6b82f
unknown <dethgrenade@gmail.com>
2020-09-22 12:30:14 -0700

    refactor <MissionDash /> to utilize message-popup modal

--- src/components/pages/MissionDash/RenderMissionDash.js
+++ src/components/pages/MissionDash/RenderMissionDash.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const RenderMissionDash = () => {
+const RenderMissionDash = ({ messagePopup }) => {
   return (
     <div className="dashboard-container">
-      <button className="mission-btn-container read">
+      <button
+        className="mission-btn-container read"
+        onClick={() => messagePopup('This feature is not available yet!')}
+      >
         <input className="progress-check" type="checkbox" />
         <div className="mission-btn-icon">
           <img src="/images/read-icon.png" alt="" />
@@ -11,14 +14,20 @@ const RenderMissionDash = () => {
         </div>
       </button>
       <div className="right-panel">
-        <button className="mission-btn-container write">
+        <button
+          className="mission-btn-container write"
+          onClick={() => messagePopup('This feature is not available yet!')}
+        >
           <input className="progress-check" type="checkbox" />
           <div className="mission-btn-icon">
             <img src="/images/write-icon.png" alt="" />
             <h3>WRITE</h3>
           </div>
         </button>
-        <button className="mission-btn-container draw">
+        <button
+          className="mission-btn-container draw"
+          onClick={() => messagePopup('This feature is not available yet!')}
+        >
           <input className="progress-check" type="checkbox" />
           <div className="mission-btn-icon">
             <img src="/images/draw-icon.png" alt="" />
