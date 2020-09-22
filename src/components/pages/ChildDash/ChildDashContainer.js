953c39ece9ed134b9453c591af62595fbba8a1ca
unknown <dethgrenade@gmail.com>
2020-09-22 08:24:49 -0700

    added info modal from Ant Design to <ChildDash />

--- src/components/pages/ChildDash/ChildDashContainer.js
+++ src/components/pages/ChildDash/ChildDashContainer.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { Modal } from 'antd';
 
 import RenderChildDash from './RenderChildDash';
 
 const ChildDashContainer = props => {
   // sets state held in <App />
   const { setHeaderTitle } = props;
+
+  const [displayMessage, setDisplayMessage] = useState(false);
 
   const history = useHistory();
 
@@ -14,7 +18,20 @@ const ChildDashContainer = props => {
     setHeaderTitle('Story Squad');
   }, [setHeaderTitle]);
 
-  return <RenderChildDash history={history} />;
+  // displays a modal with a message to the user
+  const messagePopup = () => {
+    return Modal.info({
+      title: '',
+      content: (
+        <div>
+          <p>This feature is not available yet!</p>
+        </div>
+      ),
+      onOk() {},
+    });
+  };
+
+  return <RenderChildDash history={history} messagePopup={messagePopup} />;
 };
 
 export default ChildDashContainer;
--- src/components/pages/ChildDash/RenderChildDash.js
+++ src/components/pages/ChildDash/RenderChildDash.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const RenderChildDash = ({ history }) => {
+const RenderChildDash = ({ history, messagePopup }) => {
   return (
     <div className="dashboard-container">
       <button
@@ -14,7 +14,7 @@ const RenderChildDash = ({ history }) => {
       <button
         className="dash-btn-container avatar"
         /* Feature not implemented yet. if a user clicks the button an alert is displayed with the feedback */
-        onClick={() => alert('Feature not availble yet')}
+        onClick={messagePopup}
       >
         <img src="/images/avatar-btn.png" alt="change avatar artwork" />
       </button>
@@ -22,7 +22,7 @@ const RenderChildDash = ({ history }) => {
       <button
         className="dash-btn-container passport"
         /* Feature not implemented yet. if a user clicks the button an alert is displayed with the feedback */
-        onClick={() => alert('Feature not availble yet')}
+        onClick={messagePopup}
       >
         <img src="/images/passport-btn.png" alt="adventure passport artwork" />
       </button>
@@ -30,7 +30,7 @@ const RenderChildDash = ({ history }) => {
       <button
         className="dash-btn-container trophy"
         /* Feature not implemented yet. if a user clicks the button an alert is displayed with the feedback */
-        onClick={() => alert('Feature not availble yet')}
+        onClick={messagePopup}
       >
         <img src="/images/trophy-btn.png" alt="trophy room artwork" />
       </button>
