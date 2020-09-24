efeca1fdd1b4727619632627040458098ef36605
fuston05 <fuston05@yahoo.com>
2020-09-24 18:53:20 -0400

    fixse failing test in PINForm.test.js'

--- src/__tests__/PINForm.test.js
+++ src/__tests__/PINForm.test.js
@@ -4,6 +4,15 @@ import PINForm from '../components/common/accountPinModal/PINForm';
 
 describe('PINForm component', () => {
   test('renders in the dom', () => {
+    window.localStorage.setItem(
+      'okta-token-storage',
+      JSON.stringify({
+        idToken: {
+          value:
+            'eyJraWQiOiJxaUhsZllkbzFzSTEtRnJ6LXNBZUtMT0dEY0lDM2R2d2tpOXNXMkhPNGk0IiwiYWxnIjoiUlMyNTYifQ',
+        },
+      })
+    );
     const { getByLabelText } = render(
       <PINForm
         formSubmissionData={{
