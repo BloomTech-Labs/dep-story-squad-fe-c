2f489c9ba5720d77b5805192cb50a8f82b2c8ae6
unknown <dethgrenade@gmail.com>
2020-09-29 13:50:03 -0700

    built skeleton of <MissionWrite /> component

---
+++ src/components/pages/MissionWrite/MissionWriteContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RenderMisisonWrite from './RenderMissionWrite';
+
+const MissionWriteContainer = () => {
+  // stateful logic goes here
+
+  return <RenderMisisonWrite />;
+};
+
+export default MissionWriteContainer;
---
+++ src/components/pages/MissionWrite/RenderMissionWrite.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RenderMissionWrite = props => {
+  // no stateful logic allowed. only use props passed from container component
+
+  return <></>;
+};
+
+export default RenderMissionWrite;
