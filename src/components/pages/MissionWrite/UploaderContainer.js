496f0ea3c1e71cd115a855a18bf66d8fc709ea02
fuston05 <fuston05@yahoo.com>
2020-10-01 17:07:26 -0400

    destructures out "push" from react-router-dom, addressing codeClimate issue

--- src/components/pages/MissionWrite/UploaderContainer.js
+++ src/components/pages/MissionWrite/UploaderContainer.js
@@ -10,7 +10,7 @@ import { useHistory } from 'react-router-dom';
 import { uploadSubmissionData, getData } from '../../../api';
 
 const Uploader = () => {
-  const history = useHistory();
+  const { push } = useHistory();
   const [fileList, setFileList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -62,8 +62,7 @@ const Uploader = () => {
     uploadSubmissionData(endpoint, formData, curUserToken)
       .then(res => {
         setIsLoading(false);
-        history.push('/mission');
-        console.log('uploadRes: ', res);
+        push('/mission');
       })
       .catch(err => {
         console.log('Upload Failed: ', err.message);
