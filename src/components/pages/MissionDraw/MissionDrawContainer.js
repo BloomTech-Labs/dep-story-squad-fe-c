b5adbedcbd55665d8cb0486a7c73d1af7312649f
unknown <dethgrenade@gmail.com>
2020-10-05 15:29:53 -0700

    created initial skeleton for <MissionDraw />

---
+++ src/components/pages/MissionDraw/MissionDrawContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RenderMissionDraw from './RenderMissionDraw';
+
+const MissionDrawContainer = () => {
+  return <RenderMissionDraw />;
+};
+
+export default MissionDrawContainer;
---
+++ src/components/pages/MissionDraw/RenderMissionDraw.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RenderMissionDraw = props => {
+  return <></>;
+};
+
+export default RenderMissionDraw;
