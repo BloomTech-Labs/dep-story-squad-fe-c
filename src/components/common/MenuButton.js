5301b0b10e65cd96265e9aa0ab99a63a691608cd
unknown <dethgrenade@gmail.com>
2020-10-05 10:24:47 -0700

    added onClick to <MenuButton /> to open menu

--- src/components/common/MenuButton.js
+++ src/components/common/MenuButton.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const MenuButton = () => {
+  // Refs to menu bars
+  const refBarOne = useRef();
+  const refBarTwo = useRef();
+  const refBarThree = useRef();
+
+  // "Opens/Closes" nav menu in <Header /> when clicked
+  const onClick = () => {
+    // Get the current Refs to the menu bars
+    const barOne = refBarOne.current;
+    const barTwo = refBarTwo.current;
+    const barThree = refBarThree.current;
+
+    // Toggle the class names to trigger CSS animations
+    barOne.classList.toggle('open');
+    barTwo.classList.toggle('open');
+    barThree.classList.toggle('open');
+  };
+
   return (
-    <div className="menu-btn">
-      <div id="bar-one" className="menu-bar"></div>
-      <div id="bar-two" className="menu-bar"></div>
-      <div id="bar-three" className="menu-bar"></div>
+    <div className="menu-btn" onClick={onClick}>
+      <div id="bar-one" ref={refBarOne} className="menu-bar"></div>
+      <div id="bar-two" ref={refBarTwo} className="menu-bar"></div>
+      <div id="bar-three" ref={refBarThree} className="menu-bar"></div>
     </div>
   );
 };
