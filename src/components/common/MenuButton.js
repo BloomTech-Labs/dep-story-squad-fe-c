1424133d97db8cd42d4649d9bde7e14aa2491081
unknown <dethgrenade@gmail.com>
2020-10-05 09:41:22 -0700

    added basic skeleton of menu button component

---
+++ src/components/common/MenuButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MenuButton = () => {
+  return (
+    <div className="menu-btn">
+      <span id="bar-one" className="menu-bar"></span>
+      <span id="bar-two" className="menu-bar"></span>
+      <span id="bar-three" className="menu-bar"></span>
+    </div>
+  );
+};
+
+export default MenuButton;
