e3f2bcd15f2be930c443760ae83ad1d8e8776ccf
unknown <dethgrenade@gmail.com>
2020-10-05 15:41:17 -0700

    <MissionDraw /> updated with reusable <Uploader />

--- src/components/pages/MissionDraw/MissionDrawContainer.js
+++ src/components/pages/MissionDraw/MissionDrawContainer.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import RenderMissionDraw from './RenderMissionDraw';
 
-const MissionDrawContainer = () => {
-  return <RenderMissionDraw />;
+import { useLocalStorage } from '../../../utils/hooks';
+
+const MissionDrawContainer = ({ setHeaderTitle }) => {
+  // Current user's Id for submitting files to API
+  const [userId] = useLocalStorage('curUserId');
+  // File limit controls the number of images that can be uploaded
+  // 5 for a Story submission and 1 for a Illustration
+  const fileLimit = 1;
+  // The endpoint to send the submission to when making a POST request
+  const uploadURL = `child/${userId}/mission/draw`;
+
+  // sets the header title
+  useEffect(() => {
+    setHeaderTitle('Ready, Set... Draw!');
+  }, [setHeaderTitle]);
+
+  return <RenderMissionDraw fileLimit={fileLimit} uploadURL={uploadURL} />;
 };
 
 export default MissionDrawContainer;
--- src/components/pages/MissionDraw/RenderMissionDraw.js
+++ src/components/pages/MissionDraw/RenderMissionDraw.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Uploader from '../../common/Uploader/UploaderContainer';
 
-const RenderMissionDraw = props => {
-  return <></>;
+const RenderMissionDraw = ({ fileLimit, uploadURL }) => {
+  return (
+    <>
+      <p className="text-box">
+        Draw one scene from Chapters 1-2. For a caption to your picture, write
+        out the sentence from the main story where your picture should appear.
+        Use whatever coloring supplies you want on a loose-leaf sheet of paper,
+        then take a photo and upload it to Story Squad.
+      </p>
+      <Uploader fileLimit={fileLimit} uploadURL={uploadURL} />
+    </>
+  );
 };
 
 export default RenderMissionDraw;
