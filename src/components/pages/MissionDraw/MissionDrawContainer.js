b4a8267bca9673f67a037fab7cd301d6264a7311
unknown <dethgrenade@gmail.com>
2020-10-06 08:10:39 -0700

    refactored text prompt on <MissionDraw /> to be retrieved from API

--- src/components/pages/MissionDraw/MissionDrawContainer.js
+++ src/components/pages/MissionDraw/MissionDrawContainer.js
@@ -1,23 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import RenderMissionDraw from './RenderMissionDraw';
+
+import { getData } from '../../../api';
 
 import { useLocalStorage } from '../../../utils/hooks';
 
 const MissionDrawContainer = ({ setHeaderTitle }) => {
   // Current user's Id for submitting files to API
   const [userId] = useLocalStorage('curUserId');
+  // Current user's token
+  const [curUserToken] = useLocalStorage('curUserToken', null);
   // File limit controls the number of images that can be uploaded
   // 5 for a Story submission and 1 for a Illustration
   const fileLimit = 1;
   // The endpoint to send the submission to when making a POST request
   const uploadURL = `child/${userId}/mission/draw`;
+  // Text to be displayed in the mission prompt text box
+  const [missionPrompt, setMissionPrompt] = useState('Loading...');
+
+  // Get the text for mission prompt
+  useEffect(() => {
+    getData(`child/${userId}/mission`, curUserToken).then(res => {
+      setMissionPrompt(res.data.draw);
+    });
+  }, [setMissionPrompt]);
 
   // sets the header title
   useEffect(() => {
     setHeaderTitle('Ready, Set... Draw!');
   }, [setHeaderTitle]);
 
-  return <RenderMissionDraw fileLimit={fileLimit} uploadURL={uploadURL} />;
+  return (
+    <RenderMissionDraw
+      fileLimit={fileLimit}
+      uploadURL={uploadURL}
+      missionPrompt={missionPrompt}
+    />
+  );
 };
 
 export default MissionDrawContainer;
--- src/components/pages/MissionDraw/RenderMissionDraw.js
+++ src/components/pages/MissionDraw/RenderMissionDraw.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import Uploader from '../../common/Uploader/UploaderContainer';
 
-const RenderMissionDraw = ({ fileLimit, uploadURL }) => {
+const RenderMissionDraw = ({ fileLimit, uploadURL, missionPrompt }) => {
   return (
     <>
-      <p className="text-box">
-        Draw one scene from Chapters 1-2. For a caption to your picture, write
-        out the sentence from the main story where your picture should appear.
-        Use whatever coloring supplies you want on a loose-leaf sheet of paper,
-        then take a photo and upload it to Story Squad.
-      </p>
+      <p className="text-box">{missionPrompt}</p>
       <Uploader fileLimit={fileLimit} uploadURL={uploadURL} />
     </>
   );
