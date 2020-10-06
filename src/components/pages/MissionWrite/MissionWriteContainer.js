22ceb6499502e5945abe7d26e1f43cc5737728e6
unknown <dethgrenade@gmail.com>
2020-10-06 08:13:43 -0700

    refactored text prompt on <MissionWrite /> to be fetched from API

--- src/components/pages/MissionWrite/MissionWriteContainer.js
+++ src/components/pages/MissionWrite/MissionWriteContainer.js
@@ -1,22 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import RenderMisisonWrite from './RenderMissionWrite';
+
+import { getData } from '../../../api';
+
 import { useLocalStorage } from '../../../utils/hooks';
 
 const MissionWriteContainer = ({ setHeaderTitle }) => {
   // Current user's Id for submitting files to API
   const [userId] = useLocalStorage('curUserId');
+  // Current user's token
+  const [curUserToken] = useLocalStorage('curUserToken', null);
   // File limit controls the number of images that can be uploaded
   // 5 for a Story submission and 1 for a Illustration
   const fileLimit = 5;
   // The endpoint to send the submission to when making a POST request
   const uploadURL = `child/${userId}/mission/write`;
+  // Text to be displayed in the mission prompt text box
+  const [missionPrompt, setMissionPrompt] = useState('Loading...');
+
+  // Get the text for mission prompt
+  useEffect(() => {
+    getData(`child/${userId}/mission`, curUserToken).then(res => {
+      setMissionPrompt(res.data.write);
+    });
+  }, [setMissionPrompt]);
 
   // sets the header title
   useEffect(() => {
     setHeaderTitle('Pencils Ready?');
   }, [setHeaderTitle]);
 
-  return <RenderMisisonWrite fileLimit={fileLimit} uploadURL={uploadURL} />;
+  return (
+    <RenderMisisonWrite
+      fileLimit={fileLimit}
+      uploadURL={uploadURL}
+      missionPrompt={missionPrompt}
+    />
+  );
 };
 
 export default MissionWriteContainer;
--- src/components/pages/MissionWrite/RenderMissionWrite.js
+++ src/components/pages/MissionWrite/RenderMissionWrite.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Uploader from '../../common/Uploader/UploaderContainer';
 
-const RenderMissionWrite = ({ fileLimit, uploadURL }) => {
+const RenderMissionWrite = ({ fileLimit, uploadURL, missionPrompt }) => {
   // no stateful logic allowed. only use props passed from container component
 
   return (
     <>
-      <p className="text-box">
-        Imagine and write down a scene from Finn’s Little League tryouts. (In
-        case you’re wondering, flipperball is a lot like soccer, and your job is
-        to imagine how Finn performed as the coaches looked on.) Write your
-        story with a pencil and no more than 5 sheets of loose-leaf paper, then
-        take a photo and upload it to Story Squad.
-      </p>
+      <p className="text-box">{missionPrompt}</p>
       <Uploader fileLimit={fileLimit} uploadURL={uploadURL} />
     </>
   );
