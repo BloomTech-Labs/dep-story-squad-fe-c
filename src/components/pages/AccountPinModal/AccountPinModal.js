bd73b4f8782fb122d0997a8bfa1647d8d75de1ca
fuston05 <fuston05@yahoo.com>
2020-10-07 14:22:51 -0400

    fixes AccountPinModal test file

--- src/components/pages/AccountPinModal/AccountPinModal.js
+++ src/components/pages/AccountPinModal/AccountPinModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Modal } from 'antd';
 import { getLogin } from '../../../api';
 import { useLocalStorage } from '../../../utils/hooks';
