5651fb18b1d1070111e672e075f8f5435c3dc238
fuston05 <fuston05@yahoo.com>
2020-10-08 19:23:38 -0400

    styles the pin form

--- src/components/pages/AccountPinModal/PinForm/RenderPinForm.js
+++ src/components/pages/AccountPinModal/PinForm/RenderPinForm.js
@@ -3,29 +3,29 @@
 import React from 'react';
 import PinInput from 'react-pin-input';
 import PropTypes from 'prop-types';
+import Column from 'antd/lib/table/Column';
 
 const RenderPinForm = ({ formSubmissionData, handleChange }) => {
   return (
     <div className="pinFormCont">
-      <label>
-        Enter PIN:
-        <PinInput
-          formValue={formSubmissionData.pin}
-          length={4}
-          focus
-          initialValue=""
-          // hide as you enter pin
-          secret
-          onChange={(value, index) => {
-            handleChange(value);
-          }}
-          type="numeric"
-          inputMode="number"
-          inputStyle={{ borderColor: '#6CEAE5' }}
-          inputFocusStyle={{ borderColor: 'blue' }}
-          autoSelect={true}
-        />
-      </label>
+      <label>Enter PIN:</label>
+      <PinInput
+        name="pinInput"
+        formValue={formSubmissionData.pin}
+        length={4}
+        focus
+        initialValue=""
+        // hide as you enter pin
+        secret
+        onChange={(value, index) => {
+          handleChange(value);
+        }}
+        type="numeric"
+        inputMode="number"
+        inputStyle={{ borderColor: '#6CEAE5' }}
+        inputFocusStyle={{ borderColor: 'blue' }}
+        autoSelect={true}
+      />
     </div>
   );
 };
