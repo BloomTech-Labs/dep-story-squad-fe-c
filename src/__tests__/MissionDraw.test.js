216d58cd259889a0426a5ca85b50d1861c143956
unknown <dethgrenade@gmail.com>
2020-10-12 11:02:52 -0700

    updated tests to mock useLocation() from react-router-dom

--- src/__tests__/MissionDraw.test.js
+++ src/__tests__/MissionDraw.test.js
@@ -4,6 +4,13 @@ import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    pathname: 'localhost:3000/mission/write',
+  }),
+}));
+
 describe('<MissionDraw /> test suite', () => {
   test('renders without errors', () => {
     render(
