caa4964550aa407ea6c315c4391301a0cd8b9a11
scott fuston <fuston05@yahoo.com>
2020-10-19 18:35:27 -0400

    replaces 'react-pin-input' with a custom pin input component
    Co-authored-by: jessemarek

--- src/components/pages/AccountPinModal/PinForm/PinFormContainer.js
+++ src/components/pages/AccountPinModal/PinForm/PinFormContainer.js
@@ -1,37 +1,105 @@
 // pin form container. Logic goes here
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+// recoil
+import { useRecoilState } from 'recoil';
+import { currentUserState } from '../../../../state/userState';
 
 // components
 import RenderPinForm from './RenderPinForm';
 
+// API
+import { getAccount } from '../../../../api';
+
 const PinFomContainer = ({
-  formSubmissionData,
-  setFormSubmissionData,
-  mainSubmit,
+  curUserType,
+  curUserId,
   setValidationError,
+  history,
 }) => {
-  const handleChange = value => {
-    // sends form data to modal component
-    setFormSubmissionData({
-      ...formSubmissionData,
-      pin: value,
-    });
+  const [formData, setFormData] = useState(['', '', '', '']);
+  const [formString, setFormString] = useState(formData.join(''));
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
+
+  // references to the input elements
+  const ref_1 = useRef();
+  const ref_2 = useRef();
+  const ref_3 = useRef();
+
+  const nextFocus = {
+    '0': ref_1.current,
+    '1': ref_2.current,
+    '2': ref_3.current,
+  };
+
+  const handleSubmit = () => {
+    if (formString.length === 4) {
+      const url = `/${curUserType}/${curUserId}`;
+      getAccount(url, formString)
+        .then(res => {
+          // fire Recoil selector to set localstorage and state
+          setCurrentUser({
+            ...currentUser,
+            curUserToken: res.data.token,
+            missionProgress: res.data.mission_progress,
+          });
+
+          history.push('/dashboard');
+        })
+        .catch(err => {
+          if (err) {
+            setValidationError('Error: Invalid PIN');
+          }
+        });
+    }
+  };
+
+  const changeHandler = e => {
+    const { name, value } = e.target;
+    // restrict to nums(0-9) only
+    if (value.match(/^[0-9]*$/)) {
+      setFormData(
+        formData.map((ele, i) => {
+          let temp = ele;
+          if (i === parseInt(name)) {
+            temp = value;
+          }
+          return temp;
+        })
+      );
+      setFormString(formString + value);
+
+      // increment focus
+      if (name in nextFocus) {
+        nextFocus[name].focus();
+      }
+    }
   };
 
   useEffect(() => {
-    if (formSubmissionData.pin && formSubmissionData.pin.length === 4) {
-      mainSubmit();
-    }
-  }, [formSubmissionData, mainSubmit]);
+    handleSubmit();
+  }, [formData, handleSubmit]);
 
   return (
     <>
       <RenderPinForm
-        formSubmissionData={formSubmissionData}
-        handleChange={handleChange}
+        ref_1={ref_1}
+        ref_2={ref_2}
+        ref_3={ref_3}
+        formData={formData}
+        formString={formString}
+        changeHandler={changeHandler}
       />
     </>
   );
+};
+
+RenderPinForm.propTypes = {
+  curUserType: PropTypes.string,
+  setValidationError: PropTypes.func,
+  curUserId: PropTypes.string,
+  history: PropTypes.object,
 };
 
 export default PinFomContainer;
