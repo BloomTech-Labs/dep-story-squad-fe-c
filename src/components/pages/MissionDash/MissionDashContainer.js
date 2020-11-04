b430c625b39b422abe28c10907cbe7efd093f36a
Shaun Lee <shaundlee.0103@gmail.com>
2020-11-04 13:38:56 -0500

    Removes dependancy from array in useEffect to remove infinite calls to progress

--- src/components/pages/MissionDash/MissionDashContainer.js
+++ src/components/pages/MissionDash/MissionDashContainer.js
@@ -41,7 +41,7 @@ const MissionDashContainer = () => {
       .catch(err => {
         console.log(err);
       });
-  }, [curUser, curUserId, curUserType, setCurUser]);
+  }, [curUser, curUserId, curUserType]);
 
   // Checks if mission requirements have been met and then pushes
   // to mission URL or displays message popup with the requirements
