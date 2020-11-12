91c7987d9f628b1d8701d74bfd5a3e346ba5a10d
Yi Lun Weng <info@yilunweng.com>
2020-11-11 17:16:38 -0800

    fix: component not rendered inside RecoilRoot error

--- src/__tests__/RenderChildDash.test.js
+++ src/__tests__/RenderChildDash.test.js
@@ -1,9 +1,17 @@
 import RenderChildDash from '../components/pages/ChildDash/RenderChildDash';
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 describe('<RenderChildDash /> test suite', () => {
   test('renders without errors', () => {
-    render(<RenderChildDash />);
+    render(
+      <Router>
+        <RecoilRoot>
+          <RenderChildDash />
+        </RecoilRoot>
+      </Router>
+    );
   });
 });
